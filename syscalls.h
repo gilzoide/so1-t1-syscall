1945a7cc30bfef6dedf264be61206765e529f12b
gilzoide <gilzoide@gmail.com>
2014-10-16 00:07:56 -0300

    PONY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! xD

--- syscalls.h
+++ syscalls.h
@@ -866,6 +866,8 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 			 unsigned long idx1, unsigned long idx2);
 asmlinkage long sys_finit_module(int fd, const char __user *uargs, int flags);
+
+// Nossa syscall
+asmlinkage int sys_print_a_pony(void);
 #endif
 
-asmlinkage int sys_print_a_pony(void);
